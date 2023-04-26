5367912d0ae3533d4278b0b6a17892eae2068dd7
WebDevelopo11 <si.sada93@gmail.com>
2023-04-26 23:59:19 +0200

    Init project

---
+++ migrations/m230426_215636_seed_user.php
@@ -0,0 +1,45 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m230426_215636_seed_user
+ */
+class m230426_215636_seed_user extends Migration
+{
+   /**
+    * {@inheritdoc}
+    */
+   public function safeUp()
+   {
+      $currentTime = time();
+
+      $this->insert('{{%user}}', [
+         'username' => 'luana',
+         'auth_key' => Yii::$app->security->generateRandomString(),
+         'password_hash' => Yii::$app->security->generatePasswordHash('password1234'),
+         'password_reset_token' => Yii::$app->security->generateRandomString(),
+         'email' => 'luana@email.it',
+         'created_at' => $currentTime,
+         'updated_at' => $currentTime,
+      ]);
+
+      $this->insert('{{%user}}', [
+         'username' => 'maria',
+         'auth_key' => Yii::$app->security->generateRandomString(),
+         'password_hash' => Yii::$app->security->generatePasswordHash('password5678'),
+         'password_reset_token' => Yii::$app->security->generateRandomString(),
+         'email' => 'maria@email.it',
+         'created_at' => $currentTime,
+         'updated_at' => $currentTime,
+      ]);
+   }
+
+   /**
+    * {@inheritdoc}
+    */
+   public function safeDown()
+   {
+      return false;
+   }
+}
