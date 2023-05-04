3ecab48865e92f2a09174b674dd4c88d1e6f8a75
WebDevelopo11 <si.sada93@gmail.com>
2023-05-04 23:45:12 +0200

    Added customer table migration

---
+++ migrations/m230504_202741_create_customer_table.php
@@ -0,0 +1,43 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Handles the creation of table `{{%customer}}`.
+ */
+class m230504_202741_create_customer_table extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+        $this->createTable('{{%customer}}', [
+            'id' => $this->primaryKey(),
+            'name' => $this->string()->notNull(),
+            'region' => $this->integer()->notNull(),
+            'currency' => $this->string()->notNull(),
+            'created_at' => $this->integer()->notNull(),
+            'updated_at' => $this->integer(),
+        ]);
+
+        $this->createIndex(
+            '{{idx-customer-region}}',
+            '{{%customer}}',
+            'region'
+        );
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        $this->dropIndex(
+            '{{idx-customer-region}}',
+            '{{%customer}}'
+        );
+
+        $this->dropTable('{{%customer}}');
+    }
+}
